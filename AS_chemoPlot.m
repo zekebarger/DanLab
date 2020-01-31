e5da9aab321066e8461046ef929c5aec125bd299
Zeke Barger <56006729+zekebarger@users.noreply.github.com>
2020-01-31 12:14:36 -0800

    Correct statistical tests

--- AS_chemoPlot.m
+++ AS_chemoPlot.m
@@ -1,45 +1,29 @@
-% zeke barger 010720
-% Compare sleeping behavior between control and treatment groups over
-% long time scales (for example, chemogenetic manipulations)
-%
-% NOTE: the statistics are not quite ready for prime time, since
-% there's no correction for multiple comparisons--just a
-% paired t-test at each timepoint. 
-% For the bout length histogram, bouts are pooled across days to make a
-% histogram for each mouse. Then the histograms are averaged across mice.
-% Right now, the 'histogram' is shown as a line plot. This can be easily
-% changed - overlapping transparent histograms might be preferable.
-%
+% Zeke Barger, 013120
+% plot brain states following chemogenetic manipulation
+% NOTE: the saline and CNO mouselists should point to the same mice,
+% in the same order!
+% Significance is calculated using a two-way repeated measures ANOVA
+% with Bonferroni correction followed by paired t-tests
+% *: p < 0.05, **: p < 0.01, ***: p < 0.001
+% It's assumed that each recording starts right after the injection
 
 function [] = AS_chemoPlot()
 %% user-defined parameters
-hrs2take = 3; % hours of data to take from the beginning of each recording
-epochLength = 2.5; % length of brain state epochs, in seconds
-timebin = 20; % minutes of brain states to avg for each plotted timepoint
-controlLabel = 'Saline'; % label for control experiments (e.g., 'saline')
-treatmentLabel = 'CNO'; % label for treatment experiments (e.g., 'CNO')
-plotOrder = [3 2 1]; % order to plot the brain states
-                     % 1 = REM, 2 = wake, 3 = NREM
-% the two parameters below are the times, in minutes, between which to
-% collect data for the histogram of bout durations (a bout is an 
-% uninterrupted period of some brain state)
-histoStart = 0; % time to start, in minutes
-histoEnd = 180; % time to end, in minutes
-hBinWidth = 10; % bin width for histogram, in seconds
-hMax = 3*60; % maximum bout length in the histogram, in seconds
+% how many hrs of data to take from the beginning of each recording
+hours_to_plot = 3;
+epoch_length = 2.5; % in seconds
+mins_per_bin = 20; % minutes of brain states to avg for each plotted timepoint
+plot_order = [3 2 1]; % 1=R, 2=W, 3=N. So, to plot wake, then NREM, then REM,
+%                       set this to [2 3 1]
+recording_delay = 5; % minutes between injection and recording start
 
-%% extract data from recordings
-% convert from hours/minutes to epochs
-nb = round(hrs2take*60*60/epochLength); % epochs from beginning
-tb = timebin*60/epochLength; % epochs per timepoint
-h1 = max([round(histoStart*60/epochLength), 1]); % epoch to start histogram
-h2 = round(histoEnd*60/epochLength); % epoch to stop histogram
+%% get brain state data
+% number of bins to take - don't change this
+total_epochs = round(hours_to_plot*60*60/epoch_length); % bins from beginning
+epochs_per_bin = mins_per_bin*60/epoch_length; % bins per timepoint
 
-
-% have the user select the mouselists for each type of experiment
-[sFileName,sPathName,~] = uigetfile('*.mat','Choose CONTROL (e.g., saline) mouselist');
-[cFileName,cPathName,~] = uigetfile([sPathName, '*.mat'],...
-    'Choose TREATMENT (e.g., CNO) mouselist');
+[sFileName,sPathName,~] = uigetfile('*.mat','Choose saline mouselist');
+[cFileName,cPathName,~] = uigetfile([sPathName,'*.mat'],'Choose CNO mouselist');
 
 % check if something was selected
 if ~ischar(sFileName) || ~ischar(cFileName)
@@ -56,11 +40,9 @@ if isempty(sList.mouselist) || isempty(cList.mouselist) % nothing in the list
     return
 end
 
-% load brain state data and bout lengths
-[sData,sBouts] = loadData(sList.mouselist,nb,tb,h1,h2);
-[cData,cBouts] = loadData(cList.mouselist,nb,tb,h1,h2);
-sData = sData*100;
-cData = cData*100;
+% load brain state data
+sData = loadData(sList.mouselist,total_epochs,epochs_per_bin)*100;
+cData = loadData(cList.mouselist,total_epochs,epochs_per_bin)*100;
 
 % if there was some problem
 if isempty(sData) || isempty(cData)
@@ -71,166 +53,165 @@ end
 sAvg = mean(sData,3);
 cAvg = mean(cData,3);
 
-% find variability (SEM)
+% find variability
 sSEM = std(sData,[],3)/sqrt(size(sData,3));
 cSEM = std(cData,[],3)/sqrt(size(cData,3));
 
-% get bout length histograms
-edges = 0:hBinWidth:hMax; % compute histogram edges
-nBins = length(edges) - 1; % get number of bins
-centers = edges(1:end-1) + hBinWidth/2; % find centers of bins
-% preallocate for the histograms from each mouse
-sHistos = zeros(3,nBins,size(sBouts,2));
-cHistos = zeros(3,nBins,size(cBouts,2));
-% calculate histogram for each mouse
-for i = 1:3 % for each state
-    for j = 1:size(sBouts,2)
-        [sHistos(i,:,j),~] = histcounts(sBouts{i,j}, edges,'Normalization','probability');
-    end
-    for j = 1:size(cBouts,2)
-        [cHistos(i,:,j),~] = histcounts(cBouts{i,j}, edges,'Normalization','probability');
-    end
-end
-
-% average across mice
-sHistAvg = mean(sHistos,3);
-cHistAvg = mean(cHistos,3);
-
-% find variability (SEM)
-sHistSEM = std(sHistos,[],3)/sqrt(size(sBouts,3));
-cHistSEM = std(cHistos,[],3)/sqrt(size(cBouts,3));
-
-%% plot the brain state likelihoods
+%% plot
 % set up time axis
-t = (timebin * (1:(nb/tb)) +5 - timebin/2) / 60 ;
+t = (mins_per_bin * (1:(total_epochs/epochs_per_bin)) + recording_delay - mins_per_bin/2) / 60 ;
 
 titles = {'REM','Wake','NREM'};
 colors = [43 160 220;
     129 131 132
     255 194 61] ./ 255; % rem wake nrem
-
-figure('Color','w','Position',[458 408 1010 240])
-plotCount = 1; % keeps track of how many plots we've made so far
-plotHandles = [0 0 0]; % hold handles to our plots
-for i = plotOrder
-    plotHandles(i) = subplot(1,3,plotCount); % make a subplot
-    
+%%
+figure('Color','w','Position',[458 408 1070 268])
+% keep track of how many plots we've made
+plot_number = 1;
+for i = plot_order %  for each state
+    subplot(1,3,plot_number)
+    plot_handles_c = []; % hold handles for when we make the legend
+    plot_handles_s = [];
     % plot mean and sem
-    errorbar(t,sAvg(i,:),sSEM(i,:),'o-','Color',colors(i,:),...
-        'CapSize',0,'MarkerFaceColor','w',...
-        'LineWidth',1);
-    hold on, errorbar(t,cAvg(i,:),cSEM(i,:),'o-','Color',...
-        colors(i,:),'MarkerFaceColor',colors(i,:),...
-        'LineWidth',1,'CapSize',0);
+    plot_handles_s(1) = errorbar(t,sAvg(i,:),sSEM(i,:),'o-','Color',colors(i,:),...
+        'CapSize',0,'MarkerFaceColor','w','LineWidth',1);
+    hold on, plot_handles_c(1) = errorbar(t,cAvg(i,:),cSEM(i,:),'o-','Color',colors(i,:),...
+        'MarkerFaceColor',colors(i,:),'LineWidth',1,'CapSize',0);
     
-    % show significance for each timepoint
-    for x = 1:length(t)
-        [~,p] = ttest(squeeze(sData(i,x,:)),squeeze(cData(i,x,:)));
-        
-        if p > 0.05 || isnan(p)
-            continue
-        else
-            if p<=.05
-                mark = '*';
-            end
-            if p<.01
-                mark = '**';
-            end
-            if p < .001
-                mark = '***';
-            end
+    if i == 1
+        % create legend, by first drawing placeholder lines
+        hold on, plot_handles_s(2) = errorbar([-1 -2],[0 0],[0 0],'o-','Color',...
+            colors(2,:),'CapSize',0,'MarkerFaceColor','w','LineWidth',1);
+        hold on, plot_handles_c(2) = errorbar([-1 -2],[0 0],[0 0],'o-','Color',colors(2,:),...
+            'MarkerFaceColor',colors(2,:),'LineWidth',1,'CapSize',0);
+        hold on, plot_handles_s(3) = errorbar([-1 -2],[0 0],[0 0],'o-','Color',...
+            colors(3,:),'CapSize',0,'MarkerFaceColor','w','LineWidth',1);
+        hold on, plot_handles_c(3) = errorbar([-1 -2],[0 0],[0 0],'o-','Color',colors(3,:),...
+            'MarkerFaceColor',colors(3,:),'LineWidth',1,'CapSize',0);
+        [~,lh] = legend([plot_handles_s(plot_order(1)),plot_handles_c(plot_order(1)),...
+            plot_handles_s(plot_order(2)),plot_handles_c(plot_order(2)),...
+            plot_handles_s(plot_order(3)),plot_handles_c(plot_order(3))],...
+            {'','','','','Saline','CNO'},'Location','north','Box','off',...
+            'FontSize',12,'NumColumns',3,'Position',[0.8,0.72,0.095,0.552]);
+        % get objects in the legend and rearrange them ;)
+        all_obj = {};
+        for ob = 1:6
+            all_obj{ob} = lh(ob+6).Children.Children;
         end
-        yl = ylim;
-        y=max([sAvg(i,x)+sSEM(i,x), cAvg(i,x)+cSEM(i,x)])+.03*diff(yl);
-        
-        text(t(x), y,mark,'HorizontalAlignment','center','FontSize',12)
-        
+        cno_x1 = all_obj{6}(1).XData;
+        cno_y1 = all_obj{6}(1).YData;
+        cno_x2 = all_obj{6}(2).XData;
+        cno_y2 = all_obj{6}(2).YData;
+        sal_x1 = all_obj{5}(1).XData;
+        sal_y1 = all_obj{5}(1).YData;
+        sal_x2 = all_obj{5}(2).XData;
+        sal_y2 = all_obj{5}(2).YData;
+        x_shift = 1.1*diff(cno_x2);
+        all_obj{1}(1).YData = sal_y1;
+        all_obj{1}(2).YData = sal_y2;
+        all_obj{1}(1).XData = all_obj{1}(1).XData - 2*x_shift;
+        all_obj{1}(2).XData = all_obj{1}(2).XData - 2*x_shift;
+        all_obj{2}(1).YData = cno_y1;
+        all_obj{2}(2).YData = cno_y2;
+        all_obj{2}(1).XData = all_obj{2}(1).XData - 2*x_shift;
+        all_obj{2}(2).XData = all_obj{2}(2).XData - 2*x_shift;
+        all_obj{3}(1).YData = sal_y1;
+        all_obj{3}(2).YData = sal_y2;
+        all_obj{3}(1).XData = all_obj{3}(1).XData - x_shift;
+        all_obj{3}(2).XData = all_obj{3}(2).XData - x_shift;
+        all_obj{4}(1).YData = cno_y1;
+        all_obj{4}(2).YData = cno_y2;
+        all_obj{4}(1).XData = all_obj{4}(1).XData - x_shift;
+        all_obj{4}(2).XData = all_obj{4}(2).XData - x_shift;
     end
     
-    % add a legend
-    if plotCount==1
-        legend({controlLabel,treatmentLabel},'Location','northeast','Box','off')
+    if plot_number==1 % show y label
         ylabel('Percentage (%)')
     end
-    title(titles{i},'Color',colors(i,:))
+    title(titles{i},'Color',colors(i,:),'FontSize',13)
     xlabel('Time (h)')
+    xlim([0 hours_to_plot])
+    set(gca,'FontSize',12)
     box off
     
-    % set lower y limit to 0
     yl = ylim;
-    set(gca,'YLim',[0 yl(2)]);
-    
-    % set y axis for wake and nrem to be the same
-    if plotCount == 3 % if both wake and nrem have been plotted
-        % get existing y limits
-        wY = ylim(plotHandles(2));
-        nY = ylim(plotHandles(3));
-        % get max value for upper limits
-        yNew = [0 max([wY(2) nY(2)])];
-        % apply new values
-        ylim(plotHandles(2), yNew);
-        ylim(plotHandles(3), yNew);
+    if yl(2) > 20
+        yl(2) = 100;
+    else
+        yl(2) = 20;
     end
-    plotCount = plotCount + 1; % update number of plots
-end
-
-%% plot the bout length histograms
-figure('Color','w','Position',[458 90 1010 240])
-plotCount = 1; % keeps track of how many plots we've made so far
-plotHandles = [0 0 0]; % hold handles to our plots
-for i = plotOrder
-    plotHandles(i) = subplot(1,3,plotCount); % make a subplot
+    ylim([0 yl(2)])
+    % check that number of mice and timepoints are equal
+    % (the timepoints should be, for sure. mice # could
+    % vary if there's a problem with the data).
     
-    % plot mean and sem
-    errorbar(centers,sHistAvg(i,:),sHistSEM(i,:),'o-','Color',colors(i,:),...
-        'CapSize',0,'MarkerFaceColor','w',...
-        'LineWidth',1);
-    hold on, errorbar(centers,cHistAvg(i,:),cHistSEM(i,:),'o-','Color',...
-        colors(i,:),'MarkerFaceColor',colors(i,:),...
-        'LineWidth',1,'CapSize',0);
+    if size(sData,2) ~= size(cData,2) || size(sData,3) ~= size(cData,3)
+        disp('different number of mice in each condition.')
+        disp('cannot calculate statistics')
+        continue
+    end
     
-    % show significance for each timepoint
-    for x = 1:length(centers)
-        [~,p] = ttest(squeeze(sHistos(i,x,:)),squeeze(cHistos(i,x,:)));
-        
-        if p > 0.05 || isnan(p)
-            continue
-        else
-            if p<=.05
-                mark = '*';
-            end
-            if p<.01
-                mark = '**';
-            end
-            if p < .001
-                mark = '***';
-            end
+    % perform two-way repeated measures ANOVA to determine whether there is
+    % a significant interaction of treatment and time.
+    % first, arrange the data properly
+    y = zeros(size(sData,2)*size(sData,3)*2, 1); % each measurement
+    subject = y; % subject id
+    g1 = y; % treatment
+    g2 = y; % time
+    ii = 1; % just a counter for how many datapoints we've collected
+    for j = 1:size(sData,2) % for each timepoint
+        for k = 1:size(sData,3) % for each subject
+            y(ii) = sData(i,j,k); % get the measurement
+            subject(ii) = k; % store the subject id
+            g1(ii) = 1; % set treatment to 1
+            g2(ii) = j; % store timepoint
+            ii = ii+1;
+            % repeat for CNO
+            y(ii) = cData(i,j,k); % get the measurement
+            subject(ii) = k; % store the subject id
+            g1(ii) = 2; % set treatment to 2
+            g2(ii) = j; % store timepoint
+            ii = ii+1;
         end
-        yl = ylim;
-        y=max([sHistAvg(i,x)+sHistSEM(i,x), cHistAvg(i,x)+cHistSEM(i,x)])+.03*diff(yl);
-        
-        text(centers(x), y,mark,'HorizontalAlignment','center','FontSize',12)
-        
     end
-    
-    % add a legend
-    if plotCount==1
-        legend({controlLabel,treatmentLabel},'Location','northeast','Box','off')
-        ylabel('Percentage (%)')
-    end
-    title(titles{i},'Color',colors(i,:))
-    xlabel('Bout length (sec)')
-    ylabel('Fraction of bouts')
-    box off
-    
-    plotCount = plotCount + 1; % update number of plots
+    % run the ANOVA
+    a = rm_anova2(y, subject, g1, g2, {'treatment','time'});
+    df1 = num2str(a{4,3});
+    df2 = num2str(a{7,3});
+    F = num2str(a{4,5});
+    pval = a{4,6};
+    pval_string = num2str(pval);
+    disp([titles{i},': F(',df1,',',df2,') = ',F,', p = ',pval_string])
+    % if there was a significant treatment x time interaction, run post-hoc
+    % tests at each timepoint
+    if pval <= (0.05 / 3) % Bonferroni correction
+        for x = 1:length(t)
+            [~,p] = ttest(squeeze(sData(i,x,:)),squeeze(cData(i,x,:)));       
+            if p > 0.05 || isnan(p)
+                continue
+            else
+                if p<=.05
+                    mark = '*';
+                end
+                if p<.01
+                    mark = '**';
+                end
+                if p < .001
+                    mark = '***';
+                end
+            end
+            yl = ylim;
+            y=max([sAvg(i,x)+sSEM(i,x), cAvg(i,x)+cSEM(i,x)])+.028*diff(yl);
+            text(t(x), y,mark,'HorizontalAlignment','center','FontSize',14)         
+        end
+    end 
+    plot_number = plot_number + 1;
 end
-
-
-%% function to load data from files
+%%
 % return a matrix: state x time x mouse
 % average across days for each mouse
-function [brainStateData, bouts] = loadData(mouselist,nb,tb,h1,h2)
+function [data] = loadData(mouselist,nb,tb)
 % load data
 nMice = length(mouselist); % how many mice there are
 allMiceFiles = cell(1, nMice); % locations of files
@@ -252,28 +233,15 @@ for m = 1:nMice % for each mouse
     nDays(m) = length(dirlist);
 end
 
-% our first output: state X time (binned) X mouse
-brainStateData = zeros(3,nb/tb,nMice);
-% second output: state X mouse
-bouts = cell(3,nMice);
+data = zeros(3,nb/tb,length(mouselist));
 % for each mouse
 for i = 1:nMice
     % for each day
-    % brain state likelihood, state X time (binned) X recording
+    % brain state likelihood at each timepoint, each day
     binnedState = zeros(3,nb/tb,nDays(i));
-    % preallocate bouts
-    for q = 1:3
-        bouts{q, i} = [];
-    end
     for j = 1:nDays(i)
         % load brain state data
         load(allMiceFiles{i}{j}{1},'labels');
-        % check if the recording is long enough
-        if length(labels) < nb || length(labels) < h2
-           disp(['Error: ',allMiceFiles{i}{j}{1},' is too short.']) 
-           brainStateData = [];
-           return
-        end
         % extract just the brain state for the appropriate length of time
         state = labels(1:nb);
         % get brain state likelihood at each timepoint
@@ -281,20 +249,81 @@ for i = 1:nMice
             for p = 1:3
                 binnedState(p,k,j) = sum(state(((k-1)*tb+1):(k*tb))==p)/tb;
             end
-        end 
-        % get bouts
-        state = labels(h1:h2); % just get brain state for the required time
-        if ~isrow(state) % convert to a row
-            state = state';
-        end
-        stateStr = erase(num2str(state),' '); % convert to a string
-        for q = 1:3 % for each state
-            % get start and end indices of each bout
-            [startIndex,endIndex] = regexp(stateStr,[num2str(q),'+']);
-            % add to the list of bout lengths
-            bouts{q,i} = [bouts{q,i}, endIndex - startIndex + 1];
         end
     end
-    % average across recordings
-    brainStateData(:,:,i) = mean(binnedState,3);
+    
+    data(:,:,i) = mean(binnedState,3);
 end
+
+function stats = rm_anova2(Y,S,F1,F2,FACTNAMES)
+% https://www.mathworks.com/matlabcentral/fileexchange/6874-two-way-repeated-measures-anova
+% Aaron Schurger (2005.02.04) Derived from Keppel & Wickens (2004) "Design and Analysis" ch. 18
+stats = cell(4,5);
+F1_lvls = unique(F1);
+F2_lvls = unique(F2);
+Subjs = unique(S);
+a = length(F1_lvls); % # of levels in factor 1
+b = length(F2_lvls); % # of levels in factor 2
+n = length(Subjs); % # of subjects
+INDS = cell(a,b,n); % this will hold arrays of indices
+CELLS = cell(a,b,n); % this will hold the data for each subject X condition
+MEANS = zeros(a,b,n); % this will hold the means for each subj X condition
+for i=1:a % F1
+    for j=1:b % F2
+        for k=1:n % Subjs
+            INDS{i,j,k} = find(F1==F1_lvls(i) & F2==F2_lvls(j) & S==Subjs(k));
+            CELLS{i,j,k} = Y(INDS{i,j,k});
+            MEANS(i,j,k) = mean(CELLS{i,j,k});
+        end
+    end
+end
+AB = reshape(sum(MEANS,3),a,b); % across subjects
+AS = reshape(sum(MEANS,2),a,n); % across factor 2
+BS = reshape(sum(MEANS,1),b,n); % across factor 1
+A = sum(AB,2); % sum across columns, so result is ax1 column vector
+B = sum(AB,1); % sum across rows, so result is 1xb row vector
+S = sum(AS,1); % sum across columns, so result is 1xs row vector
+T = sum(sum(A)); % could sum either A or B or S, choice is arbitrary
+dfA = a-1;
+dfB = b-1;
+dfAB = (a-1)*(b-1);
+dfS = n-1;
+dfAS = (a-1)*(n-1);
+dfBS = (b-1)*(n-1);
+dfABS = (a-1)*(b-1)*(n-1);
+expA = sum(A.^2)./(b*n);
+expB = sum(B.^2)./(a*n);
+expAB = sum(sum(AB.^2))./n;
+expS = sum(S.^2)./(a*b);
+expAS = sum(sum(AS.^2))./b;
+expBS = sum(sum(BS.^2))./a;
+expY = sum(sum(sum(MEANS.^2))); %sum(Y.^2);
+expT = T^2 / (a*b*n);
+ssA = expA - expT;
+ssB = expB - expT;
+ssAB = expAB - expA - expB + expT;
+ssS = expS - expT;
+ssAS = expAS - expA - expS + expT;
+ssBS = expBS - expB - expS + expT;
+ssABS = expY - expAB - expAS - expBS + expA + expB + expS - expT;
+ssTot = expY - expT;
+msA = ssA / dfA;
+msB = ssB / dfB;
+msAB = ssAB / dfAB;
+msS = ssS / dfS;
+msAS = ssAS / dfAS;
+msBS = ssBS / dfBS;
+msABS = ssABS / dfABS;
+fA = msA / msAS;
+fB = msB / msBS;
+fAB = msAB / msABS;
+pA = 1-fcdf(fA,dfA,dfAS);
+pB = 1-fcdf(fB,dfB,dfBS);
+pAB = 1-fcdf(fAB,dfAB,dfABS);
+stats = {'Source','SS','df','MS','F','p';...
+    FACTNAMES{1}, ssA, dfA, msA, fA, pA;...
+    FACTNAMES{2}, ssB, dfB, msB, fB, pB;...
+    [FACTNAMES{1} ' x ' FACTNAMES{2}], ssAB, dfAB, msAB, fAB, pAB;...
+    [FACTNAMES{1} ' x Subj'], ssAS, dfAS, msAS, [], [];...
+    [FACTNAMES{2} ' x Subj'], ssBS, dfBS, msBS, [], [];...
+    [FACTNAMES{1} ' x ' FACTNAMES{2} ' x Subj'], ssABS, dfABS, msABS, [], []};
